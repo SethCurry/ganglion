fe232185778fd3ecae4595f969f158d4ff2675f9
Seth Curry <scurry@atlassian.com>
2026-04-27 17:44:27 -0400

    Adding a frontmatter library

---
+++ lib/frontmatter.ts
@@ -0,0 +1,34 @@
+import fs from "fs";
+
+interface Frontmatter {
+  [key: string]: any;
+}
+
+interface SplitFrontmatterResult<T> {
+  frontmatter: T;
+  content: string;
+}
+
+export function splitFrontmatter<T>(
+  content: string,
+): SplitFrontmatterResult<T> {
+  const frontmatterRegex = /^---\n(.*?)\n---\n/;
+  const frontmatterMatch = content.match(frontmatterRegex);
+  if (frontmatterMatch) {
+    return {
+      frontmatter: JSON.parse(frontmatterMatch[1]!) as T,
+      content: content.replace(frontmatterRegex, ""),
+    };
+  }
+  return {
+    frontmatter: {} as T,
+    content: content,
+  };
+}
+
+export function parseFrontMatterFromFile<T>(
+  filePath: string,
+): SplitFrontmatterResult<T> {
+  const content = fs.readFileSync(filePath, "utf8");
+  return splitFrontmatter<T>(content);
+}
